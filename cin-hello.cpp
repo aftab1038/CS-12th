96b6a768ff11e3900c400bec76d3f3d0fb519cf1
Aftab nafees <138874348+aftab1038@users.noreply.github.com>
2023-08-06 12:50:03 +0500

    Add files via upload

---
+++ cin-hello.cpp
@@ -0,0 +1,16 @@
+/* Use of Cin function to get name from user and 
+   then Cout hello with that name.*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cout<<"what's your name: ";
+    cin>>name;
+    cout<< "Hello, "<<name<<"!\n";
+
+    return 0;
+}
