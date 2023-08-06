a179aca051591ab2ef1ba95a5ad401e006f2d76d
Aftab nafees <138874348+aftab1038@users.noreply.github.com>
2023-08-06 12:39:49 +0500

    Add files via upload

---
+++ prac-getchar.cpp
@@ -0,0 +1,16 @@
+// Today task is to use the getchar function 
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Are you student(y or n): ";
+    char ch= getchar();                 
+
+    cout<<"Your are eligible.\n";
+    return 0;
+    
+}
