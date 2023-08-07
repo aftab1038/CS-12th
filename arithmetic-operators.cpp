2963d0d5850633220ac22facfdc571d2ffb321cf
Aftab nafees <138874348+aftab1038@users.noreply.github.com>
2023-08-07 17:07:15 +0500

    Add files via upload

---
+++ arithmetic-operators.cpp
@@ -0,0 +1,123 @@
+/* 
+    Arithmetic Operation
+     1. Addition(+)          2. Subtraction(-)
+     3. Multiplication(*)    4. Division(/)
+     5. Modulus(%)   
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// Declaration of functions for every operation
+float sum();
+float subt();
+float product();
+float div();
+int mod();
+
+// Main function
+int main()              
+{
+// Function call
+    sum();
+    subt();
+    product();
+    div();
+    mod();
+
+    return 0;
+} 
+
+
+
+// Function for Addition
+float sum()
+{
+    float a,b,sum;
+
+    cout<<"Input numbers for addition\n"
+        <<"Number 1: ";
+    cin>>a;
+    cout<<"Number 2: ";
+    cin>>b;
+
+    sum=a+b;
+
+    cout<<a<<"+"<<b<<" = "<<sum<<endl;
+
+    return sum;
+}
+
+// Function for Subtraction
+float subt()
+{
+    float a,b,subt;
+
+    cout<<"Input numbers for subtraction\n"
+        <<"Number 1: ";
+    cin>>a;
+    cout<<"Number 2: ";
+    cin>>b;
+
+    subt=a-b;
+
+    cout<<a<<"-"<<b<<" = "<<subt<<endl;
+
+    return subt;
+}
+
+// Function for Multiplication
+float product()
+{
+    float a,b,prod;
+
+    cout<<"Input numbers for multiplication\n"
+        <<"Number 1: ";
+    cin>>a;
+    cout<<"Number 2: ";
+    cin>>b;
+
+    prod=a*b;
+
+    cout<<a<<"*"<<b<<" = "<<prod<<endl;
+
+    return prod;
+}
+
+// Function for Division
+float div()
+{
+    float a,b,div;
+
+    cout<<"Input numbers for division\n"
+        <<"Number 1: ";
+    cin>>a;
+    cout<<"Number 2: ";
+    cin>>b;
+
+    div=a/b;
+
+    cout<<a<<"/"<<b<<" = "<<div<<"\n";
+
+    return div;
+}
+
+// Function for Modulus
+int mod()
+{
+    int a,b;
+    int mod;
+
+    cout<<"Input numbers for finding remainder(only integers, not decimal)\n"
+        <<"Number 1: ";
+    cin>>a;
+    cout<<"Number 2: ";
+    cin>>b;
+
+    mod=a%b;
+
+    cout<<"Remainder of "<<a<<"/"<<b<<": "<<mod<<"\n";
+
+    return mod;
+}
