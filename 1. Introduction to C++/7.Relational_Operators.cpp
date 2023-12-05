e9405ac10b480d865187f18b12fc8fa03e4eed93
Aftab nafees <138874348+aftab1038@users.noreply.github.com>
2023-12-05 17:48:01 +0500

    Relational Operators usage

---
+++ 1. Introduction to C++/7.Relational_Operators.cpp
@@ -0,0 +1,30 @@
+/*  Relational operator.....     */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    
+    cout<<"input the value of num1: ";
+    cin>>a;
+    cout<<"input the value of num2: ";
+    cin>>b;
+    
+    if (a>b)                            //  > Greater than
+    {
+        cout<<a<<" greatar than "<<b<<endl;
+    }
+    else if (a<b)                      //   < Less than
+    {
+        cout<<a<<" less than "<<b<<endl;
+    }
+    else if (a==b)                     //   == Equal to 
+    {
+        cout<<a<<" equal to "<<b<<endl;
+    }
+    
+    return 0;
+}
