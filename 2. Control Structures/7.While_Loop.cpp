f15fdb58f81d71b04903fa9fa587a116cfbf1474
Aftab nafees <138874348+aftab1038@users.noreply.github.com>
2023-12-19 18:52:04 +0500

    While Loop

---
+++ 2. Control Structures/7.While_Loop.cpp
@@ -0,0 +1,25 @@
+/*  While Loop
+        write a C++ program that uses while loop */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Printing sequences of number using while loop!\n";
+    int size;
+
+    cout<<"Enter a postive number: ";
+    cin>>size;
+
+    //  Printing sequences of number 
+    int i = 1;
+    while(i <= size)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+
+    return 0;
+}
