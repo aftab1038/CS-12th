677cb1e5ad5ade369bbd81ff8bd44862ea347b21
Aftab nafees <138874348+aftab1038@users.noreply.github.com>
2023-12-19 19:06:32 +0500

    Do While Loop

---
+++ 2. Control Structures/8.Do_While_Loop.cpp
@@ -0,0 +1,31 @@
+/*  Do While Loop
+        write a C++ program that uses do while loop */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //  promting User untill required number is not entered
+    int num;
+
+    do
+    {
+        cout<<"Enter a number: ";
+        cin>>num;
+
+        if (num <= 0)
+        {
+            cout<<"Sorry! You didn't enter Postive number."<<endl;
+        } 
+        
+    } while (num <= 0);
+    
+    if (num > 0)
+    {
+        cout<<"Thank You for entering postive number."<<endl;
+    }
+    
+    return 0;
+}
