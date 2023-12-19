519632663885050bf856aafe979662a424dba8c9
Aftab nafees <138874348+aftab1038@users.noreply.github.com>
2023-12-19 19:20:26 +0500

    Nested For Loop

---
+++ 2. Control Structures/9.Nested_For_Loop.cpp
@@ -0,0 +1,31 @@
+/*  Nested For Loop
+        write a program using nested for loop */
+    
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Printing Two Dimensional Block"<<endl;
+
+    int row, column;
+    cout<<"Enter the number of rows: ";
+    cin>>row;
+    cout<<"Enter the number of columns: ";
+    cin>>column;
+
+    //  Loop for printing Rows
+    for (int i = 0; i < row; i++)
+    {
+        //  Loop for printing Columns
+        for (int j = 0; j < column; j++)
+        {
+            cout<<"#";
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
