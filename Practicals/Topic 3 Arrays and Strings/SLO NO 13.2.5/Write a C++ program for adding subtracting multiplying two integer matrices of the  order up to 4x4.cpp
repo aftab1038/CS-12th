90f977efbcd5d22e0cb7ed803dbef6dced1e9e33
aftab1038 <aftabk.1038@outlook.com>
2024-02-24 21:33:57 +0500

    Practical No 8

---
+++ Practicals/Topic 3 Arrays and Strings/SLO NO 13.2.5/Write a C++ program for adding subtracting multiplying two integer matrices of the  order up to 4x4.cpp
@@ -0,0 +1,12 @@
+/*  SLO NO : 13.2.5
+    SLO Name : Write a C++ program for adding subtracting multiplying two integer matrices of the order up to 4x4. */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+
+    return 0;
+}
