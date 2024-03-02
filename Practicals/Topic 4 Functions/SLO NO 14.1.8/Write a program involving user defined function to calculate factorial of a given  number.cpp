3ee6350c10d419b9a26e0e93153a04d316040e88
aftab1038 <aftabk.1038@outlook.com>
2024-03-02 21:15:15 +0500

    Practical NO 13

---
+++ Practicals/Topic 4 Functions/SLO NO 14.1.8/Write a program involving user defined function to calculate factorial of a given  number.cpp
@@ -0,0 +1,51 @@
+/*  SLO NO. : 14.1.8
+    SLO Name : Write a program involving user defined function to calculate factorial of a given number. */
+
+#include <iostream>
+
+using namespace std;
+
+// Function Calling
+void Factorial(int num);
+
+int main()
+{
+    cout<<"Factorial Calculator...."<<endl;
+
+    int num;
+    cout<<"Enter the Number : ";
+    cin>>num;
+
+    // Factorial only exist for 0 and postive number
+    if (num >= 0)
+    {
+        Factorial(num);
+    }
+
+     // Factorial of Negative number is undefined
+    else if (num < 0)
+    {
+        cout<<"Factorial of "<<num<<" : Undefined"<<endl;
+    }
+    
+    // if user only any other kind of input
+    else
+    {
+        cout<<"Invalid Input!!"<<endl;
+    }
+
+    return 0;
+}
+
+// Function for factorial calculator
+void Factorial(int num)
+{
+    int factorial=1;
+    for (int i = 1; i<=num; i++)
+    {
+        factorial = factorial * i;
+    }
+    
+    cout<<"Factorial of "<<num<<" : "<<factorial<<endl;
+}
+
