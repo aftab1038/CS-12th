1c3b56d2a7e5e3b60611a4b0969fc4661d96c5b0
aftab1038 <aftabk.1038@outlook.com>
2024-03-18 09:31:18 +0500

    Solution for Past paper Questions

---
+++ Past Papers/2023/Q7_b.cpp
@@ -0,0 +1,28 @@
+// Code by AFTAB NAFEES
+// GitHub : aftab1038
+
+/* 2023 Paper Questions 
+    Q7 b. Write a C++ program to display n terms of natural numbers and their sum  */
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    int sum = 0, n;
+    cout<<"Enter a ending number : ";
+    cin>>n;
+
+    // printing numbers till n and adding these numbers 
+    for (int i = 1; i<=n; i++)
+    {
+        cout<<i<<endl;
+        sum += i;   
+    }
+
+    // printing sum 
+    cout<<"Sum of these natural Numbers : "<<sum<<endl;
+    return 0;
+}
